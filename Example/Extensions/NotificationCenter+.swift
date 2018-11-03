ff455b36d8f92033cdf2c531ba10a003346ab82b
wpcfan <peng.wang@outlook.com>
2018-11-03 14:56:32 +0800

    add notification extensions

---
+++ Example/Extensions/NotificationCenter+.swift
@@ -0,0 +1,9 @@
+//
+//  NotificationCenter+.swift
+//  Example
+//
+//  Created by 王芃 on 2018/11/3.
+//  Copyright © 2018 twigcodes. All rights reserved.
+//
+
+import Foundation
---
+++ Example/Extensions/Rx/Reactive+NotificationCenter.swift
@@ -0,0 +1,9 @@
+//
+//  Reactive+NotificationCenter.swift
+//  Example
+//
+//  Created by 王芃 on 2018/11/3.
+//  Copyright © 2018 twigcodes. All rights reserved.
+//
+
+import Foundation
