296b0f2848bb9e77aa4e95d9df32d130ed13ba2c
wpcfan <peng.wang@outlook.com>
2018-11-12 23:33:37 +0800

    add smartcloud dep

---
+++ Example/Extensions/AppDelegate+JdSmartCloud.swift
@@ -0,0 +1,9 @@
+//
+//  AppDelegate+JdSmartCloud.swift
+//  Example
+//
+//  Created by 王芃 on 2018/11/12.
+//  Copyright © 2018 twigcodes. All rights reserved.
+//
+
+import Foundation
---
+++ Example/Services/JdSmartCloudService.swift
@@ -0,0 +1,9 @@
+//
+//  JdSmartCloudService.swift
+//  Example
+//
+//  Created by 王芃 on 2018/11/12.
+//  Copyright © 2018 twigcodes. All rights reserved.
+//
+
+import Foundation
