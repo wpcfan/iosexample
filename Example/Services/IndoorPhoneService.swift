04f2f3ffcb04ba0a2e9a1c0b4f1442b032fb4f28
wpcfan <peng.wang@outlook.com>
2019-01-14 20:58:25 +0800

    update files

--- Example/Services/IndoorPhoneService.swift
+++ Example/Services/IndoorPhoneService.swift
@@ -2,8 +2,26 @@
 //  IndoorPhoneService.swift
 //  Example
 //
-//  Created by 王芃 on 2019/1/14.
-//  Copyright © 2019 twigcodes. All rights reserved.
+//  Created by 王芃 on 2018/11/13.
+//  Copyright © 2018 twigcodes. All rights reserved.
 //
 
-import Foundation
+class IndoorPhoneService {
+    func initVideo() -> Void {
+        #if TARGET_CPU_ARM
+        DongDongManager.share()?.initVideo()
+        #endif
+    }
+    
+    func startVideo(_ with: UIView, remoteIp: String, remotePort: Int, localPort: Int) -> Void {
+        #if TARGET_CPU_ARM
+        DongDongManager.share()?.startVideo(with: with, remoteIp: remoteIp, remotePort: remotePort, localPort: localPort)
+        #endif
+    }
+    
+    func stopVideo() -> Void {
+        #if TARGET_CPU_ARM
+        DongDongManager.share()?.stopVideo()
+        #endif
+    }
+}
