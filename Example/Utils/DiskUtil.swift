a0930b59f5697caca4472646d0991321c764b497
wpcfan <peng.wang@outlook.com>
2019-02-13 14:05:11 +0800

    restructure

---
+++ Example/Utils/DiskUtil.swift
@@ -0,0 +1,9 @@
+//
+//  DiskUtil.swift
+//  Example
+//
+//  Created by 王芃 on 2019/2/13.
+//  Copyright © 2019 twigcodes. All rights reserved.
+//
+
+import Foundation
---
+++ Example/Views/PasswordField.swift
@@ -0,0 +1,13 @@
+//
+//  PasswordView.swift
+//  Example
+//
+//  Created by 王芃 on 2019/2/13.
+//  Copyright © 2019 twigcodes. All rights reserved.
+//
+
+import Foundation
+
+class PasswordView: UIText {
+    <#code#>
+}
