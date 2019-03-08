47cf89c49b5d24a4416cce650fd83c8fe1255f49
wpcfan <peng.wang@outlook.com>
2019-03-08 14:17:55 +0800

    restructure

---
+++ Example/Domain/JD/JdActivateResult.swift
@@ -0,0 +1,9 @@
+//
+//  JdActivateResult.swift
+//  Example
+//
+//  Created by 王芃 on 2019/3/7.
+//  Copyright © 2019 twigcodes. All rights reserved.
+//
+
+import Foundation
---
+++ Example/Extensions/Rx/Reactive+UITextField.swift
@@ -0,0 +1,9 @@
+//
+//  Reactive+UITextField.swift
+//  Example
+//
+//  Created by 王芃 on 2019/3/8.
+//  Copyright © 2019 twigcodes. All rights reserved.
+//
+
+import Foundation
